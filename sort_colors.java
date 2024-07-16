e32407a7afaf98369ccc4a8818c3863861d003f9
Moh Aamir <mohaamir0706@gmail.com>
2024-07-16 19:03:34 +0530

    day 1 of 45days question 2 sort colors

---
+++ sort_colors.java
@@ -0,0 +1,18 @@
+package arsh_dsa_sheet;
+
+public class sort_colors {
+	public void sortColors(int[] nums) {
+        int n = nums.length;
+        for(int i = 1; i<n;++i){
+            int key = nums[i];
+            int j = i-1;
+            while(j>=0 && nums[j]>key){
+                nums[j+1] = nums[j];
+                j = j-1;
+            }
+            nums[j+1]=key;
+        }
+        
+    }
+
+}
