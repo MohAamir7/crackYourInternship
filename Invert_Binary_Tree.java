2b0aaa7aaa57e3dccaf4f1e025808ef495f23288
Moh Aamir <mohaamir0706@gmail.com>
2024-07-17 21:48:54 +0530

    day 1 of 45days question Invert Binary Tree

---
+++ Invert_Binary_Tree.java
@@ -0,0 +1,19 @@
+package arsh_dsa_sheet;
+
+public class Invert_Binary_Tree {
+	public TreeNode invertTree(TreeNode root) {
+        if(root == null){
+            return null;
+        }
+        TreeNode temp = root.left;
+        root.left = root.right;
+        root.right = temp;
+
+        invertTree(root.left);
+        invertTree(root.right);
+
+        return root;
+        
+    }
+
+}
